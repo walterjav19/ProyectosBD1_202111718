b83ece4b6fccff0ded3bf6c4da478da3439f1d21
Walter Javier <walterjav19@gmail.com>
2024-03-03 14:13:51 -0600

    fix: query 8,9

--- Proyecto_1/SQL/query8.sql
+++ Proyecto_1/SQL/query8.sql
@@ -3,4 +3,35 @@
 monto.*/
 use proyecto1; 
 
-select * from orden;
+select 
+month(O.fecha_orden) as Num_Mes,
+monthname(O.fecha_orden) as Nombre_Mes, 
+sum(O.cantidad*P.precio) as Monto_Total 
+from orden O
+join producto P on O.id_producto=P.id_producto
+join Vendedor V on O.id_vendedor=V.id_vendedor
+join Pais Pa on V.id_pais=Pa.id_pais
+where Pa.id_pais=10
+group by Num_Mes,Nombre_Mes;
+
+#si la consulta fuese de compras por mes 
+SELECT
+    EXTRACT(MONTH FROM O.fecha_orden) AS NumeroMes,
+    monthname(O.fecha_orden) as Nombre_Mes, 
+    SUM(O.cantidad * P.Precio) AS Monto
+FROM
+    Orden O
+JOIN
+    Producto P ON O.id_producto = P.id_producto
+JOIN
+    Cliente C ON O.id_cliente = C.id_cliente
+JOIN
+    Pais Pa ON C.id_pais = Pa.id_pais
+WHERE
+    Pa.nombre = 'Inglaterra'
+GROUP BY
+    NumeroMes,Nombre_Mes
+ORDER BY
+    NumeroMes;
+
+
--- Proyecto_1/SQL/query9.sql
+++ Proyecto_1/SQL/query9.sql
@@ -1,3 +1,26 @@
 /*Mostrar el mes con más y menos ventas. Se debe de mostrar el número de mes y
 monto. (Una sola consulta).*/
 use proyecto1;
+(select month(O.fecha_orden) as Num_Mes,
+monthname(O.fecha_orden) as Nombre_Mes, 
+sum(O.cantidad*P.precio) as Monto_Total 
+from orden O
+join producto P on O.id_producto=P.id_producto
+join Vendedor V on O.id_vendedor=V.id_vendedor
+join Pais Pa on V.id_pais=Pa.id_pais
+group by Num_Mes,Nombre_Mes
+order by Monto_Total desc
+limit 1)
+union
+(select month(O.fecha_orden) as Num_Mes,
+monthname(O.fecha_orden) as Nombre_Mes, 
+sum(O.cantidad*P.precio) as Monto_Total 
+from orden O
+join producto P on O.id_producto=P.id_producto
+join Vendedor V on O.id_vendedor=V.id_vendedor
+join Pais Pa on V.id_pais=Pa.id_pais
+group by Num_Mes,Nombre_Mes
+order by Monto_Total asc
+limit 1);
+
+
